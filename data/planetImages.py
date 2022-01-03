6fa978f794a8acfae767a0b3df175b4ce251d02a
Camilo Enrique Martínez Forero <caemartinezfo@unal.edu.co>
2022-01-03 14:39:02 -0500

    Creación de jupyter notebook para la visualización y descarga de imágenes de Earth Engine

--- data/planetImages.py
+++ data/planetImages.py
@@ -3,5 +3,12 @@ import ee
 # Initialize the Earth Engine module.
 ee.Initialize()
 
-# Print metadata for a DEM dataset.
+# Acceso a la colección de imágenes de Planet
 nicfi = ee.ImageCollection('projects/planet-nicfi/assets/basemaps/americas')
+
+# Filtra la primera imagen con el método first 
+basemap = nicfi.filter(ee.Filter.date('2021-03-01','2021-07-01')).first()
+print(type(basemap))
+
+# Resolución espacial de las imágenes Planet
+print(nicfi.get('spatial_resolution').getInfo())
